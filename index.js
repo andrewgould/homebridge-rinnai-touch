fcb105885bb1761217cf6d503909f15118918dd0
Andrew Gould <gould.andrew@gmail.com>
2020-01-26 11:52:45 +1100

    Fixing some bugs, though it's still not yet functioning

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ function RinnaiTouch(log, config) {
 	this.values.Active = Characteristic.Active.INACTIVE;
 	this.values.CurrentTemperature = null;
 	this.values.ThresholdTemperature = null;
-	this.values.RotationSpeed = 0;
 }
 
 RinnaiTouch.prototype = {
@@ -32,6 +31,18 @@ RinnaiTouch.prototype = {
 	identify: function(callback) {
 		this.log("identify");
 		callback();
+	},
+
+	refreshData: function(apiOutput) {
+		const spawn = require("child_process").spawn;
+		const pythonProcess = spawn('python',["node_modules/rinnai-touch-python-interface/rinnai-touch-status.py", this.ip]);
+		
+		pythonProcess.stdout.on('data', function(dataRaw) {
+			// {"Status": { "System": { "EvapMode":false,"HeaterMode":false,"SystemOn": true },"Heater": { "HeaterOn":false,"FanSpeed": 0,"CirculationFanOn": false,"AutoMode": false,"ManualMode": false,"SetTemp": 0,"ZoneA": false,"ZoneB": false,"ZoneC": false,"ZoneD": false},"Cooling": { "CoolingOn":true,"CirculationFanOn": true,"AutoMode": false,"ManualMode": true,"SetTemp": 20,"ZoneA": true,"ZoneB": false,"ZoneC": false,"ZoneD": false},"Evap": { "EvapOn":false,"FanOn": false,"FanSpeed": 0,"WaterPumpOn": false}}}
+
+			const data = JSON.parse(dataRaw);
+			apiOutput = data.Status;
+		}.bind(this));
 	},
 
 	getServices: function() {
@@ -106,20 +117,12 @@ RinnaiTouch.prototype = {
 					this.hcService.getCharacteristic(Characteristic.CurrentHeaterCoolerState).updateValue(Characteristic.CurrentHeaterCoolerState.COOLING);
 					this.hcService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(Characteristic.CurrentHeatingCoolingState.COOLING);
 					this.hcService.getCharacteristic(Characteristic.TargetHeaterCoolerState).updateValue(Characteristic.TargetHeaterCoolerState.COOL);
-					
-					this.values.CurrentTemperature = data.Cooling.SetTemp;
-					this.hcService.getCharacteristic(Characteristic.ThresholdTemperature).updateValue(this.values.CurrentTemperature);
-					this.hcService.getCharacteristic(Characteristic.CurrentTemperature).updateValue(this.values.CurrentTemperature);
 					break;
 				
 				case 'HEATING':
 					this.hcService.getCharacteristic(Characteristic.CurrentHeaterCoolerState).updateValue(Characteristic.CurrentHeaterCoolerState.HEATING);
 					this.hcService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(Characteristic.CurrentHeatingCoolingState.HEATING);
 					this.hcService.getCharacteristic(Characteristic.TargetHeaterCoolerState).updateValue(Characteristic.TargetHeaterCoolerState.HEAT);
-					
-					this.values.CurrentTemperature = data.Heating.SetTemp;
-					this.hcService.getCharacteristic(Characteristic.ThresholdTemperature).updateValue(this.values.CurrentTemperature);
-					this.hcService.getCharacteristic(Characteristic.CurrentTemperature).updateValue(this.values.CurrentTemperature);
 					break;
 				
 				default:
@@ -127,15 +130,56 @@ RinnaiTouch.prototype = {
 					this.hcService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(Characteristic.CurrentHeatingCoolingState.OFF);
 			}
 		});
+		
+		switch (CharacteristicName) {
+			case 'Active':
+				this.values.Active = this.apiOutput.System.SystemOn;
+				callback(null, this.values.Active);
+				break;
+			
+			case 'CurrentTemperature':
+				switch (data.System.CurrentMode) {
+					case 'COOLING':
+						this.values.CurrentTemperature = this.apiOutput.Cooling.SetTemp;
+						this.hcService.getCharacteristic(Characteristic.ThresholdTemperature).updateValue(this.values.CurrentTemperature);
+						this.hcService.getCharacteristic(Characteristic.CurrentTemperature).updateValue(this.values.CurrentTemperature);
+						
+						this.hcService.getCharacteristic(Characteristic.CurrentHeaterCoolerState).updateValue(Characteristic.CurrentHeaterCoolerState.COOLING);
+						this.hcService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(Characteristic.CurrentHeatingCoolingState.COOLING);
+						this.hcService.getCharacteristic(Characteristic.TargetHeaterCoolerState).updateValue(Characteristic.TargetHeaterCoolerState.COOL);
+						break;
+				
+					case 'HEATING':
+						this.values.CurrentTemperature = this.apiOutput.Heater.SetTemp;
+						this.hcService.getCharacteristic(Characteristic.ThresholdTemperature).updateValue(this.values.CurrentTemperature);
+						this.hcService.getCharacteristic(Characteristic.CurrentTemperature).updateValue(this.values.CurrentTemperature);
+
+						this.hcService.getCharacteristic(Characteristic.CurrentHeaterCoolerState).updateValue(Characteristic.CurrentHeaterCoolerState.HEATING);
+						this.hcService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(Characteristic.CurrentHeatingCoolingState.HEATING);
+						this.hcService.getCharacteristic(Characteristic.TargetHeaterCoolerState).updateValue(Characteristic.TargetHeaterCoolerState.HEAT);
+						break;
+						
+					default:
+						this.hcService.getCharacteristic(Characteristic.CurrentHeaterCoolerState).updateValue(Characteristic.CurrentHeaterCoolerState.IDLE);
+				}
+				callback(null, this.values.CurrentTemperature);
+				break;
+			
+			default:
+				callback(null);
+				break;
+		}
 	},
 
 	_setValue: function(CharacteristicName, value, callback) {
 		if(this.debug) {this.log("SET", CharacteristicName, value);}
 
 		var parameters = [];
+		var actionSet = false;
 
 		switch (CharacteristicName) {
 			case "Active":
+				actionSet = true;
 				switch (value) {
 					case Characteristic.Active.ACTIVE:
 						parameters.push('--action=on');
@@ -148,7 +192,9 @@ RinnaiTouch.prototype = {
 			break;
 
 			case "TargetHeaterCoolerState":
-				// The Panasonic API responses don't line up with what we expect
+				if (!actionSet) {
+					parameters.push('--action=on');
+				}
 				switch (value) {
 					case Characteristic.TargetHeaterCoolerState.COOL:
 						parameters.push('--mode=cool');
@@ -163,7 +209,11 @@ RinnaiTouch.prototype = {
 			break;
 
 			case "ThresholdTemperature":
-				parameters.push('--heatTemp=' + value);
+				if (!actionSet) {
+					parameters.push('--action=on');
+					parameters.push('--mode=cool');
+				}
+				// parameters.push('--heatTemp=' + value);
 				parameters.push('--coolTemp=' + value);
 			break;
 		}
